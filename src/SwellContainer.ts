e820c06faa00be031679a522b896b9081d466873
Keith Mifsud <mifsud.k@gmail.com>
2026-07-23 14:17:33 +0200

    👮 Stricter API

--- src/SwellContainer.ts
+++ src/SwellContainer.ts
@@ -1,5 +1,6 @@
 import type { BindingConfig } from './contracts/BindingConfig';
 import { BindingNotFound } from './errors/BindingNotFound';
+import { InitializableBindingConfig } from './contracts/InitializableBindingConfig';
 
 export class SwellContainer<TRegistry extends Record<string, BindingConfig<any>>> {
   private readonly registry: TRegistry;
@@ -9,10 +10,10 @@ export class SwellContainer<TRegistry extends Record<string, BindingConfig<any>>
     this.registry = registry;
   }
 
-  public static init<T extends Record<string, BindingConfig<any>>> (
-    registry: T
-  ): SwellContainer<T> {
-    return new SwellContainer(registry);
+  public static init<T extends Record<string, any>> (
+    registry: { [K in keyof T]: InitializableBindingConfig<T[K]> }
+  ): SwellContainer<{ [K in keyof T]: BindingConfig<T[K] extends { factory: () => infer R } ? R : never> }> {
+    return new SwellContainer(registry as any);
   }
 
   public resolve<K extends keyof TRegistry> (
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export { SwellContainer } from './SwellContainer';
-
